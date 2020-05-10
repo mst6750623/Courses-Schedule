be23d4bb30f6f8e1e2d1230f01caaa9b413af532
HarviMa <943941437@qq.com>
2020-05-10 12:16:06 +0800

    add class-adding module
    the class infomation display hasn't finished yet

--- front-End/js/addClass.js
+++ front-End/js/addClass.js
@@ -2,15 +2,67 @@ var app=new Vue({
     el:"#forms",
     data:{
         courses:[
-            {id:"88888888",name:"高数和线性代数基本理论概述与统计",score:"5"},
-            {id:"11111111",name:"高数2",score:"5"},
-            {id:"222",name:"线代1",score:"3"},
-            {id:"223",name:"线代2",score:"3"},
+           
+        ],
+        courseids:[
+
+        ],
+        finalCourse:[
+
+        ],
+        classId:[
+
         ]
     },
     methods:{
         confirm:function(){
-
+            var a=this.$data.finalCourse;
+           
+            var courseInfo=a.join('-');
+            if(sessionStorage.getItem('classId') == null)
+            {
+                var index="class1";
+                console.log("index1",index);
+                this.$data.classId.push(index);
+                sessionStorage.setItem("classId",this.$data.classId);
+                sessionStorage.setItem(index,courseInfo);
+            }
+            else 
+            {
+                var classId=sessionStorage.getItem("classId").split(',');
+                for(var i=0;i<classId.length;i++)
+                {
+                    this.$data.classId.push(classId[i]);
+                }
+                var num=parseInt(classId[classId.length-1].slice(5))+1;
+                console.log("num",num);
+                var index="class"+num;
+                console.log("index2",index);
+                this.$data.classId.push(index);
+                sessionStorage.setItem("classId",this.$data.classId);
+                sessionStorage.setItem(index,courseInfo);
+            }
+            //window.parent.reload();
         }
+
+    },
+    mounted:function(){
+        
+        var ids= sessionStorage.getItem("courseids");
+        if(ids != null)
+            ids=ids.split("-");
+        for(var i=0;i<ids.length;i++)
+        {
+            this.$data.courseids.push(ids[i]);
+            var courseInfo=sessionStorage.getItem(ids[i]).split('&');
+            this.$data.courses.push({id:ids[i],name:courseInfo[0]})
+        }
+        //this.$data.courseids.set(sessionStorage.getItem("courseids").split("-"));
+        console.log(JSON.stringify(this.$data.courseids));    
     }
+
+})
+
+$(document).ready(function(){
+    
 })
--- front-End/js/index.js
+++ front-End/js/index.js
@@ -21,9 +21,15 @@ var pre=new Vue({
         courseName:"",
         courseScore:"",
         courseNum:"",
-        classData:[
+        coursesData:[
 
         ],
+        finalCourseId:[
+
+        ],
+        classData:{
+            
+        },
         courseResult:[
             {id:"111",name:"高数1",score:"5"},
             {id:"111",name:"高数1",score:"5"},
@@ -121,9 +127,9 @@ var pre=new Vue({
     },
     methods:{
         addCourse:function(){
-            console.log(this.classData);
-            this.$data.classData.push({id:this.$data.courseId,name:this.$data.courseName,score:this.$data.courseScore,num:this.$data.courseNum});
-           
+            console.log(this.coursesData);
+            this.$data.coursesData.push({id:this.$data.courseId,name:this.$data.courseName,score:this.$data.courseScore,num:this.$data.courseNum});
+            this.$data.finalCourseId.push(this.$data.courseId);
             this.courseId="";
             this.courseName="";
             this.courseScore="";
@@ -131,22 +137,40 @@ var pre=new Vue({
         },
         remove:function(index){
             console.log(index);
-            this.$data.classData.splice(index,1);
+            this.$data.coursesData.splice(index,1);
+            this.$data.finalCourseId.splice(index,1);
         },
         submitCourse:function(){
            
-            console.log(JSON.stringify(this.$data.classData))
-            $.ajax({
+            console.log(JSON.stringify(this.$data.coursesData))
+            for(var i=0;i<this.$data.coursesData.length;i++)
+            {
+                var a=this.$data.coursesData[i];
+                var id=a.id;
+                var name=a.name;
+                var score=a.score;
+                var num=a.num;
+                var joins=name+'&'+score+'&'+num;
+                console.log(joins);
+                sessionStorage.setItem(id,joins);
+                console.log( sessionStorage.getItem("dat2") );
+            }
+            sessionStorage.removeItem("courseids");
+            var courseids=this.$data.finalCourseId.join('-');
+            console.log("courseids",courseids);
+            sessionStorage.setItem("courseids",courseids);
+            alert("保存信息成功！");
+           /* $.ajax({
                 type: 'POST',
                 url: 'http://127.0.0.1:5000/todo/api/v1.0/tasks',
-                data: JSON.stringify(this.$data.classData),
+                data: JSON.stringify(this.$data.coursesData),
                 contentType: "application/json", 
                 dataType:"json",
                 success: function(data){
                     console.log(data)
                 },
                 
-              });
+              });*/
         },
         addClass:function(){
             var login_div = document.createElement('iframe');
@@ -164,7 +188,38 @@ var pre=new Vue({
             $(".login_close").click(login_complete);
         },
         
+    },
+    created:function(){
+        var ids= sessionStorage.getItem("courseids");
+        if(ids != null)
+            ids=ids.split("-");
+        
+        for(var i=0;i<ids.length;i++)
+            {
+                this.$data.finalCourseId.push(ids[i]);
+                var courseInfo=sessionStorage.getItem(ids[i]).split('&');
+                this.$data.coursesData.push({id:ids[i],name:courseInfo[0],score:courseInfo[1],num:courseInfo[2]});
+            }    
+    },
+    mounted:function(){
+        if(sessionStorage.getItem("classId") != null)
+        {
+            var classes=sessionStorage.getItem("classId").split(',');
+            for(var i=0;i<classes.length;i++)
+            {
+                
+                Vue.set(this.classData, classes[i], []);
+                console.log(JSON.stringify(this.classData))
+                var classUni=this.classData.getItem
+                //this.classData.classes
+                //pre.classData = Object.assign({}, pre.classData, {
+                  //  age: 27,
+                  //  favoriteColor: 'Vue Green'
+                 // });// 方案二
+            }
+        }
     }
+
 })
 
 $(document).ready(function(){
@@ -207,7 +262,7 @@ function resultTOMiddle(){
     {
         var row=i%12;
         var col=parseInt(i/12);
-        console.log(row,col);
+        //console.log(row,col);
         pre.middleCourseResult[row][col]=pre.courseResult[i];
     }
 }
@@ -215,6 +270,4 @@ function resultTOMiddle(){
 function login_complete() {
     $(".new").remove();
     $('html,body').css('overflow', '');
-    $('#login').addClass('is_login');
-    $('#notify').hover(function(){createBox();},function(){$('.msg_box').remove();});
   }
